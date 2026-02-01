f79c5fb710bfa328b38af2abf9e2ede2754f7901
Abhishek Thulasi <abhishek45nine@gmail.com>
2026-02-01 12:42:22 +0530

    Integrated Rust and removed Android + iOS implementations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:crux/src/rust/api/simple.dart';
+import 'package:crux/src/rust/frb_generated.dart';
 
-void main() {
+Future<void> main() async {
+  await RustLib.init();
   runApp(const MainApp());
 }
 
@@ -9,11 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Center(child: Text(greet(name: 'Abhishek'))),
       ),
     );
   }
